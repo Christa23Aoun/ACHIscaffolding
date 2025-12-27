423e94bc529a29a0bf0fc062a16fb6eb47a6566d
ralphkian <ralph.kjian@hotmail.com>
2025-12-27 13:51:14 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
-import { Route, Routes } from "react-router-dom"
 import { useTranslation } from "react-i18next"
-
-import 'glider-js/glider.min.css'
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-
-import Gallery from './pages/Gallery'
-import Footer from './components/Footer'
 import ScrollToTop from './components/ScrollToTop'
-import PageNotFound from './pages/PageNotFound'
-import Blog from './pages/Blog'
-import BlogItem from './pages/BlogItem'
-import Services from './pages/Services'
-import SingleService from './components/services/SingleService'
-
-import About from './pages/About'
-import Products from './pages/Products'
-import Projects from './pages/Projects'
-import Sectors from './pages/Sectors'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   const [showMenu, setshowMenu] = useState(false)
@@ -48,36 +29,16 @@ function App() {
   return (
     <>
       <ScrollToTop />
-      <div className="App" onClick={() => showMenu ? setshowMenu(false) : true}>
-        <Header
-          handleLanguage={handleLanguage}
-          currentLanguage={currentLanguage}
-          handleCountry={handleCountry}
-          currentCountry={currentCountry}
-        />
-
-        <Routes>
-          <Route path="/" element={<Home showMenu={showMenu} setshowMenu={setshowMenu} direction={i18n.dir()} userLang={i18n.language} />} />
-
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/sectors" element={<Sectors />} />
-
-          <Route path="/services" element={<Services showMenu={showMenu} setshowMenu={setshowMenu} userLang={i18n.language} />} />
-          <Route path="/services/serviceItem" element={<SingleService />} />
-
-          <Route path="/gallery" element={<Gallery />} />
-
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog-post-2" element={<BlogItem />} />
-          <Route path="/blog-post-1" element={<BlogItem />} />
-          <Route path="/blog-post-3" element={<BlogItem />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-
-        <Footer />
-      </div>
+      <AppRoutes
+        showMenu={showMenu}
+        setshowMenu={setshowMenu}
+        userLang={i18n.language}
+        direction={i18n.dir()}
+        handleLanguage={handleLanguage}
+        currentLanguage={currentLanguage}
+        handleCountry={handleCountry}
+        currentCountry={currentCountry}
+      />
     </>
   )
 }
